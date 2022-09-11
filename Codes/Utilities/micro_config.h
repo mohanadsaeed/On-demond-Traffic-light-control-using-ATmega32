9bb7ef08e930040f43cefe7b726847e0e3f7f2c0
mohanadsaeed <mohandkhaled24@gmail.com>
2022-09-11 15:30:28 +0200

    Apply layered architecture into project's folder structure

---
+++ Codes/Utilities/micro_config.h
@@ -0,0 +1,21 @@
+/* -----------------------------------------------------------------------------------------
+[FILE NAME]    :	avr_config.h
+
+[AUTHOR]       :	MOHANAD K. SAEED
+
+[DATA CREATED] :	07/02/2021
+
+[DESCRIPTION]  :	This file contains all avr libraries 
+------------------------------------------------------------------------------------------*/
+
+#ifndef MC_CONFIG_H
+#define MC_CONFIG_H
+
+#ifndef F_CPU
+#define F_CPU 1000000UL  //1MHz Clock frequency
+#endif  
+
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+#endif
