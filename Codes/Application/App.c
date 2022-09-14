29a062f2549fd25fd69d18f78a3e6496025b52b9
mohanadsaeed <mohandkhaled24@gmail.com>
2022-09-14 22:23:23 +0200

    Application Code Update

---
+++ Codes/Application/App.c
@@ -0,0 +1,141 @@
+ /******************************************************************************
+ *
+ * Module: Application
+ *
+ * File Name: App.c
+ *
+ * Description: Source file for ATmega32 Microcontroller - Application
+ *
+ * Author: Mohanad Khaled
+ ******************************************************************************/
+#include "App.h"
+#include "../ECUAL/Button/Button.h"
+#include "../ECUAL/LED/Led.h"
+#include "../MCAL/Timer/Timer.h"
+#include "../MCAL/External Interrupt/external_interrupts.h"
+
+
+volatile uint8 flag=0,lock=0;
+static void Normal_Mode(void);
+static void crossPedestrian(void);
+static void timer_callBack(void);
+static void button_callBack(void);
+
+//Function to initialize the used drivers in the application
+void App_Init(void){
+	SET_BIT(SREG,7);
+	Button_Init();
+	Led_Init();
+	Timer1_Init(&Timer1_Configuration);
+	Timer1_setCallBack(timer_callBack,Timer1_Configuration.mode);
+	INT0_Init(&Ei_ConfigurationInt0);
+	INT0_setCallBack(button_callBack);
+}
+
+void App_Start(){
+	Normal_Mode();
+}
+
+//The normal mode for the traffic light system
+void Normal_Mode(void){
+	Timer1_startCount(Timer1_Configuration.clock);
+	while(flag!=1){
+		Led_On(LED1_CHANNEL);
+		Led_On(LED6_CHANNEL);
+	}
+	Timer1_stopCount();
+	flag=0;
+	Timer1_startCount(Timer1_Configuration.clock);
+	while(flag!=1){
+		Led_Blink(LED2_CHANNEL);
+		Led_Blink(LED5_CHANNEL);
+	}
+	Timer1_stopCount();
+	flag=0;
+	Led_Off(LED1_CHANNEL);
+	Led_Off(LED2_CHANNEL);
+	Led_Off(LED5_CHANNEL);
+	Led_Off(LED6_CHANNEL);
+	Timer1_startCount(Timer1_Configuration.clock);
+	while(flag!=1){
+		Led_On(LED3_CHANNEL);
+		Led_On(LED4_CHANNEL);
+	}
+	Timer1_stopCount();
+	flag=0;
+	Timer1_startCount(Timer1_Configuration.clock);
+	while(flag!=1){
+		Led_Blink(LED2_CHANNEL);
+		Led_Blink(LED5_CHANNEL);
+	}
+	Timer1_stopCount();
+	flag=0;
+	Led_Off(LED3_CHANNEL);
+	Led_Off(LED4_CHANNEL);
+}
+
+//Function to count 5 seconds
+void timer_callBack(void){
+	SET_BIT(SREG,7);
+	flag=1;
+}
+
+//The procedure which happened when the pedestrian push the button
+void crossPedestrian(void){
+	//wait 5 seconds
+	Timer1_startCount(Timer1_Configuration.clock);
+	while(flag!=1){
+		//Turn On Cars' Red LED
+		Led_On(LED3_CHANNEL);
+		//Turn On Pedestrian's Green LED
+		Led_On(LED4_CHANNEL);
+	}
+	Timer1_stopCount();
+	flag=0;
+	//Blinking Yellow LED for 5 seconds
+	Timer1_startCount(Timer1_Configuration.clock);
+	while(flag!=1){
+		Led_Blink(LED2_CHANNEL);
+		Led_Blink(LED5_CHANNEL);
+	}
+	Timer1_stopCount();
+	flag=0;
+	Led_Off(LED2_CHANNEL);
+	Led_Off(LED3_CHANNEL);
+	Led_Off(LED4_CHANNEL);
+	Led_Off(LED5_CHANNEL);
+}
+
+//The function which called when the pedestrian push the button to check the state of traffic flow
+void button_callBack(void){
+	SET_BIT(SREG,7);
+	if(lock==0){
+		lock=1;
+		uint8 redLed,greenLed,yellowLed;
+		redLed=Led_State(LED3_CHANNEL);
+		yellowLed=Led_State(LED2_CHANNEL);
+		greenLed=Led_State(LED1_CHANNEL);
+		if(redLed){
+			crossPedestrian();
+			lock=0;
+		}
+		else{
+			//Turn On Pedestrian Red LED
+			//Blinking Yellow LED for 5 seconds
+			Timer1_startCount(Timer1_Configuration.clock);
+			while(flag!=1){
+				Led_On(LED6_CHANNEL);
+				Led_Blink(LED2_CHANNEL);
+				Led_Blink(LED5_CHANNEL);
+			}
+			Timer1_stopCount();
+			flag=0;
+			Led_Off(LED2_CHANNEL);
+			Led_Off(LED5_CHANNEL);
+			Led_Off(LED6_CHANNEL);
+			crossPedestrian();
+			lock=0;
+		}
+	}
+}
+
---
+++ Codes/Application/App.h
@@ -0,0 +1,17 @@
+ /******************************************************************************
+ *
+ * Module: Application
+ *
+ * File Name: App.h
+ *
+ * Description: Header file for ATmega32 Microcontroller - Application
+ *
+ * Author: Mohanad K. Saeed
+ ******************************************************************************/
+#ifndef APP_H
+#define APP_H
+
+void App_Init(void);
+void App_Start(void);
+
+#endif
