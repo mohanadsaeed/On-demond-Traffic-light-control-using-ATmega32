41a92c7c1c191fca8e39b060eab35ba11b882593
mohanadsaeed <mohandkhaled24@gmail.com>
2022-09-16 09:30:02 +0200

    Update Application Code
    Bug Fix
    
    Make the ISR shorter by using flag

--- Codes/Application/App.c
+++ Codes/Application/App.c
@@ -15,8 +15,9 @@
 #include "../MCAL/External Interrupt/external_interrupts.h"
 
 
-volatile uint8 flag=0,lock=0;
+volatile uint8 timer_Flag=0,lock=0,button_Flag=0;
 static void Normal_Mode(void);
+static void prepareCrossPedestrian(void);
 static void crossPedestrian(void);
 static void timer_callBack(void);
 static void button_callBack(void);
@@ -38,77 +39,102 @@ void App_Start(){
 
 //The normal mode for the traffic light system
 void Normal_Mode(void){
+	timer_Flag=0;
 	Timer1_startCount(Timer1_Configuration.clock);
-	while(flag!=1){
-		Led_On(LED1_CHANNEL);
-		Led_On(LED6_CHANNEL);
+	Led_On(LED1_CHANNEL);
+	Led_On(LED6_CHANNEL);
+	while(timer_Flag!=1){
+		if(button_Flag!=1){
+		}
+		else{
+			prepareCrossPedestrian();
+			button_Flag=0;
+			return;
+		}
 	}
 	Timer1_stopCount();
-	flag=0;
+	timer_Flag=0;
 	Timer1_startCount(Timer1_Configuration.clock);
-	while(flag!=1){
-		Led_Blink(LED2_CHANNEL);
-		Led_Blink(LED5_CHANNEL);
+	Led_On(LED1_CHANNEL);
+	Led_On(LED6_CHANNEL);
+	while(timer_Flag!=1){
+		if(button_Flag!=1){
+			Led_Blink(LED2_CHANNEL);
+		}
+		else{
+			prepareCrossPedestrian();
+			button_Flag=0;
+			return;
+		}
 	}
 	Timer1_stopCount();
-	flag=0;
+	timer_Flag=0;
 	Led_Off(LED1_CHANNEL);
 	Led_Off(LED2_CHANNEL);
-	Led_Off(LED5_CHANNEL);
 	Led_Off(LED6_CHANNEL);
 	Timer1_startCount(Timer1_Configuration.clock);
-	while(flag!=1){
-		Led_On(LED3_CHANNEL);
-		Led_On(LED4_CHANNEL);
+	Led_On(LED3_CHANNEL);
+	Led_On(LED4_CHANNEL);
+	while(timer_Flag!=1){
+		if(button_Flag!=1){
+		}
+		else{
+			prepareCrossPedestrian();
+			button_Flag=0;
+			return;
+		}
 	}
 	Timer1_stopCount();
-	flag=0;
+	timer_Flag=0;
 	Timer1_startCount(Timer1_Configuration.clock);
-	while(flag!=1){
-		Led_Blink(LED2_CHANNEL);
-		Led_Blink(LED5_CHANNEL);
+	Led_On(LED3_CHANNEL);
+	Led_On(LED4_CHANNEL);
+	while(timer_Flag!=1){
+		if(button_Flag!=1){
+			Led_Blink(LED5_CHANNEL);
+		}
+		else{
+			prepareCrossPedestrian();
+			button_Flag=0;
+			return;
+		}
 	}
 	Timer1_stopCount();
-	flag=0;
+	timer_Flag=0;
 	Led_Off(LED3_CHANNEL);
 	Led_Off(LED4_CHANNEL);
-}
-
-//Function to count 5 seconds
-void timer_callBack(void){
-	SET_BIT(SREG,7);
-	flag=1;
+	Led_Off(LED5_CHANNEL);
 }
 
 //The procedure which happened when the pedestrian push the button
 void crossPedestrian(void){
+	Timer1_stopCount();
+	timer_Flag=0;
 	//wait 5 seconds
 	Timer1_startCount(Timer1_Configuration.clock);
-	while(flag!=1){
+	while(timer_Flag!=1){
 		//Turn On Cars' Red LED
 		Led_On(LED3_CHANNEL);
 		//Turn On Pedestrian's Green LED
 		Led_On(LED4_CHANNEL);
 	}
 	Timer1_stopCount();
-	flag=0;
+	timer_Flag=0;
 	//Blinking Yellow LED for 5 seconds
 	Timer1_startCount(Timer1_Configuration.clock);
-	while(flag!=1){
+	while(timer_Flag!=1){
 		Led_Blink(LED2_CHANNEL);
 		Led_Blink(LED5_CHANNEL);
 	}
 	Timer1_stopCount();
-	flag=0;
+	timer_Flag=0;
 	Led_Off(LED2_CHANNEL);
 	Led_Off(LED3_CHANNEL);
 	Led_Off(LED4_CHANNEL);
 	Led_Off(LED5_CHANNEL);
 }
 
-//The function which called when the pedestrian push the button to check the state of traffic flow
-void button_callBack(void){
-	SET_BIT(SREG,7);
+void prepareCrossPedestrian(void){
 	if(lock==0){
 		lock=1;
 		uint8 redLed,greenLed,yellowLed;
@@ -122,14 +148,18 @@ void button_callBack(void){
 		else{
 			//Turn On Pedestrian Red LED
 			//Blinking Yellow LED for 5 seconds
+			Timer1_stopCount();
+			timer_Flag=0;
 			Timer1_startCount(Timer1_Configuration.clock);
-			while(flag!=1){
+			while(timer_Flag!=1){
+				Led_On(LED1_CHANNEL);
 				Led_On(LED6_CHANNEL);
 				Led_Blink(LED2_CHANNEL);
 				Led_Blink(LED5_CHANNEL);
 			}
 			Timer1_stopCount();
-			flag=0;
+			timer_Flag=0;
+			Led_Off(LED1_CHANNEL);
 			Led_Off(LED2_CHANNEL);
 			Led_Off(LED5_CHANNEL);
 			Led_Off(LED6_CHANNEL);
@@ -137,5 +167,15 @@ void button_callBack(void){
 			lock=0;
 		}
 	}
+}
+
+//The function which called when the pedestrian push the button to check the state of traffic flow
+void button_callBack(void){
+	button_Flag=1;
+}
+
+//Function to count 5 seconds
+void timer_callBack(void){
+	timer_Flag=1;
 }
 
