bc73f85992ddce2d68cde2eaa9b31dbe904a5510
mohanadsaeed <mohandkhaled24@gmail.com>
2022-09-16 09:30:58 +0200

    Update Timer_stopCount Function
    Edit the function to clear the counting register

--- Codes/MCAL/Timer/Timer_Cfg.h
+++ Codes/MCAL/Timer/Timer_Cfg.h
@@ -13,8 +13,9 @@
 
 #define TimerConf_INITIAL_VALUE  		(uint16)0
 #define TimerConf_TICK					(uint16)19531
-#define TimerConf_DUTY_CYCLE_A			(uint16)
-#define TimerConf_DUTY_CYCLE_B			(uint16)
+#define TimerConf_TOP					(uint16)0
+#define TimerConf_DUTY_CYCLE_A			(uint16)0
+#define TimerConf_DUTY_CYCLE_B			(uint16)0
 #define TimerConf_CLOCK					(Timer1_Clock)TIMER1_F_CPU_256
 #define TimerConf_OC1_A_MODE			(Timer1_Oc1AMode)OC1_A_DISCONNECT
 #define TimerConf_OC1_B_MODE			(Timer1_Oc1BMode)OC1_B_DISCONNECT
